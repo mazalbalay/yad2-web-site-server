5627095dbf351b02ba99dff8b215740d428f8981
mazal balay <mazalbalay3@gmail.com>
2023-01-24 15:13:09 +0200

    upddate

--- config/config.js
+++ config/config.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb://127.0.0.1:27017/yad2",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  () => {
-    console.log("Connected to Database1");
-  },
-);
+mongoose
+  .connect(
+    "mongodb+srv://yad2:yad2@cluster0.amdm7nt.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    mongoose.set("strictQuery", false),
+  )
+  .then(() => console.log("connected"))
+  .catch((e) => console.log(e));
